17715d7286c06f27b7304257191879a552abbc5d
Shawak9 <shawaksaraf456@gmail.com>
2022-12-24 09:53:11 +0530

    Optimizing font size for phones

--- js/main.js
+++ js/main.js
@@ -189,22 +189,20 @@
 
 	var goToTop = function() 
 	{
-
 		$('.js-gotop').on('click', function(event){
 			
 			event.preventDefault();
 
 			$('html, body').animate({
 				scrollTop: $('html').offset().top
-			}, 500, 'easeInOutExpo');
-			
+			}, 250, 'easeInOutExpo');
 			return false;
 		});
 
-		$(window).scroll(function(){
-
+		$(window).scroll(function()
+		{
 			var $win = $(window);
-			if ($win.scrollTop() > 200) 
+			if ($win.scrollTop() > 700 /* && $win.onscroll() */) 
 			{
 				$('.js-top').addClass('active');
 			} else 
